3773a425f7cf24ee2a57b2e69c3f3e07a16fa2b5
dacantu <dacantu@microsoft.com>
2022-12-19 22:53:19 -0600

    Add project files.

---
+++ ClassLibrary1/MyStream.cs
@@ -0,0 +1,52 @@
+﻿using StreamSourceGenLib;
+
+namespace ClassLibrary1;
+
+[GenerateStreamBoilerplate]
+public partial class MyStream : Stream
+{
+    public override long Length => throw new NotImplementedException();
+
+    public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public override void Flush()
+    {
+        throw new NotImplementedException();
+    }
+
+    private partial int ReadCore(Span<byte> buffer)
+    {
+        // Do Read 
+        throw new NotImplementedException();
+    }
+
+    private partial ValueTask<int> ReadCoreAsync(Memory<byte> buffer, CancellationToken cancellationToken)
+    {
+        // Do ReadAsync
+        throw new NotImplementedException();
+    }
+
+    private partial long SeekCore(long offset, SeekOrigin origin)
+    {
+        // Do Seek
+        throw new NotImplementedException();
+    }
+
+    private partial void SetLengthCore(long value)
+    {
+        // Do SetLength
+        throw new NotImplementedException();
+    }
+
+    private partial void WriteCore(ReadOnlySpan<byte> buffer)
+    {
+        // Do Write
+        throw new NotImplementedException();
+    }
+
+    private partial ValueTask WriteCoreAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken)
+    {
+        // Do WriteAsync
+        throw new NotImplementedException();
+    }
+}
---
+++ StreamSourceGenLib/GenerateStreamBoilerplateAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StreamSourceGenLib
+{
+    /// <summary>
+    /// Instructs the System.IO.Stream source generator to generate boilerplate implementations for multiple legacy members, leaving the implementer only with the task of providing implementations for the core operations.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class GenerateStreamBoilerplateAttribute : Attribute
+    {
+        public bool CanRead { get; set; } = true;
+        public bool CanSeek { get; set; } = true;
+        public bool CanWrite { get; set; } = true;
+    }
+}
